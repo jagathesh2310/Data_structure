3419c8c84a5182ee7c5537d72eefffe68f193a62
jagathesh2310 <jagatheshkumar88@gmail.com>
2023-05-30 18:25:01 +0530

    commiting try

--- src/LinkedListDemo.java
+++ src/LinkedListDemo.java
@@ -1,9 +1,139 @@
+import java.util.Iterator;
 
+class LinkedList<T> implements Iterable<T>{
+    private Node head;
 
+    @Override
+    public Iterator iterator() {
+        return new Iterator() {
+            Node temp=head;
+            @Override
+            public boolean hasNext() {
+                if(temp==null){
+                    return false;
+                }
+                return true;
+            }
 
+            @Override
+            public T next() {
+                T val=temp.data;
+                temp=temp.next;
+                return val;
+            }
+        };
+    }
+
+    private class Node{
+        Node(T val){
+            data=val;
+            next=null;
+        }
+        T data;
+        Node next;
+    }
+    LinkedList(){
+        head=null;
+    }
+    public void insertAtBeginning(T val){
+        Node newNode=new Node(val);
+        if(head==null){
+            head=newNode;
+        }
+        else{
+            newNode.next=head;
+            head=newNode;
+        }
+
+    }
+    public void insertAtPos(int pos,T val){
+        if (pos==0){
+            insertAtBeginning(val);
+            return;
+        }
+        Node newNode=new Node(val);
+        Node temp=head;
+        for (int i=1;i<pos;i++){
+            temp=temp.next;
+            if(temp==null){
+                throw new IllegalArgumentException("Invalid position");
+            }
+        }
+        newNode.next=temp.next;
+        temp.next=newNode;
+    }
+    public void deleteAtBeginning(){
+        head=head.next;
+    }
+    public void deleteAtPos(int pos){
+        if(pos==0){
+            deleteAtBeginning();
+            return;
+        }
+        Node temp=head;
+        Node prev=null;
+        for (int i=1;i<=pos;i++){
+            prev=temp;
+            temp=temp.next;
+        }
+        prev.next=temp.next;
+
+    }
+    public void display(){
+        Node temp=head;
+        while (temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+    public void updateAtPos(int pos,T val){
+        Node temp=head;
+        for (int i=1;i<=pos;i++){
+            temp=temp.next;
+        }
+        temp.data=val;
+    }
+    public T get(int pos){
+        Node temp=head;
+        for (int i=1;i<=pos;i++){
+            temp=temp.next;
+        }
+        return temp.data;
+    }
+    public void reverseList(){
+        Node next;
+        Node current=head;
+        Node previous=null;
+        while (current!=null){
+            next=current.next;
+            current.next=previous;//reverse main
+            previous=current;
+            current=next;
+        }
+        head=previous;
+    }
+}
 
 public class LinkedListDemo {
     public static void main(String[] args) {
+        LinkedList<Integer> list=new LinkedList<Integer>();
+        list.insertAtBeginning(9);
+        list.insertAtBeginning(8);
+        list.insertAtBeginning(10);
+        list.insertAtBeginning(11);
+        list.insertAtPos(4,19);
+        //list.display();
+        list.deleteAtPos(2);
+        //list.display();
+        list.updateAtPos(0,90);
+        list.display();
+//        System.out.println(list.get(2));
+//        for (int i:list) {
+//            System.out.println(i);
+//        }
+        list.reverseList();
+        list.display();
 
     }
 }
